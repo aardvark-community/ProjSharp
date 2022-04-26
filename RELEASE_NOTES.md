42507f9287fc61e41a0322ce30e7646fbeaaa436
krauthaufen <krauthaufen@awx.at>
2022-04-26 23:53:46 +0200

    initial version

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,14 +1,2 @@
-### 0.0.5
-* Index exposing Rows/Cols
-
-### 0.0.4
-* copying input-data on build
-
-### 0.0.3
-* netstandard2.1
-
-### 0.0.2
-* netstandard2.0
-
 ### 0.0.1
 * initial version
--- src/Demo/Program.fs
+++ src/Demo/Program.fs
@@ -10,10 +10,10 @@ let main argv =
 
     use p = new Proj()
 
-
+    let wkt = """GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]]"""
     use t = 
         p.CreateTransformation(
-            "+proj=longlat +datum=WGS84 +no_defs", 
+            wkt, //"+proj=longlat +datum=WGS84 +no_defs", 
             "+proj=utm +zone=33 +datum=WGS84"
         )
 
